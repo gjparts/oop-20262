14006f579983360e09c460e72ffd4a0648553c37
gjparts <gjparts@gmail.com>
2026-05-14 09:50:40 -0600

    commit 140520269093

---
+++ 02_Metodos/02_Metodos/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02_Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sumar(2,3));
+            Saludar();
+            //los metodos void no se imprimen porque no retornan valor:
+            //Console.WriteLine(Saludar());
+        }
+
+        //Metodos o funciones
+        static double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+        static void Saludar()
+        {
+            Console.WriteLine("Hola");
+            Console.WriteLine("Como estan?");
+            Console.WriteLine("Un gusto");
+        }
+    }
+}
