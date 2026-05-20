95b3a19ea59074910505e525c9094c234b667083
gjparts <gjparts@gmail.com>
2026-05-20 09:59:39 -0600

    commit 2005202632549

---
+++ 04_Clases/04_Clases/Mascota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Clases
+{
+    public class Mascota
+    {
+        //Propiedades: caracteristicas de la clase
+        public String Nombre;
+        public String Especie;
+        public int Edad;
+        public char Genero;
+    }
+}
---
+++ 04_Clases/04_Clases/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _04_Clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*en C# las convenciones o notaciones para nombrar clases y objetos
+             son:
+            - utilice Pascal Case para nombrar clases,
+              La primer letra de cada palabra va en mayusculas,ejm:
+              Mascota
+              Persona
+              CuentaBancaria
+              UbicacionDeOrigen
+
+            - utilice camel Case para nombrar objetos y variables:
+              La primer letra va en minusculas y luego cada palabra
+              inicia en mayusculas, ejm:
+              m1
+              mascota1
+              cuentaBancaria42
+              docenteAsignadoCarrera
+            
+            - para nombrar objetos y variables tambien puede usar
+              la notacion snake case, consiste en escribir todo
+              en minusculas y separar cada palabra con guion bajo,
+              esta notacion se usa mas en python, ejm:
+              m1
+              mascota1
+              cuenta_bancaria_42
+              docente_asignado_carrera
+             */
+
+            //Instanciar un nuevo objeto de clase Mascota
+            Mascota m1 = new Mascota();
+            //m1 es el objeto
+            //Mascota es la clase
+
+            //asignar valores a las propiedades de m1 (set):
+            m1.Nombre = "Firulais";
+            m1.Especie = "Perro";
+            m1.Edad = 2;
+            m1.Genero = 'M';
+
+            //leer los valores de las propiedades de m1 (get):
+            Console.WriteLine("Propiedades de m1:");
+            Console.WriteLine($"Nombre: {m1.Nombre}");
+            Console.WriteLine($"Especie: {m1.Especie}");
+            Console.WriteLine($"Edad: {m1.Edad}");
+            Console.WriteLine($"Genero: {m1.Genero}");
+            Console.WriteLine($"Hash de memoria: {m1.GetHashCode()}");
+        }
+    }
+}
