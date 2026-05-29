89b89713967e6d520a7d792073eda05d8704ec49
gjparts <gjparts@gmail.com>
2026-05-29 16:10:38 -0600

    commit 2905202623121

---
+++ 04_Clases/04_Clases/Estudiante.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Clases
+{
+    public class Estudiante
+    {
+        //Campos privados
+        private string _nombre;
+        private string _cuenta;
+        private string _dni;
+        private char _genero;
+        private int _edad;
+        private float _promedio;
+        private string _carrera;
+
+        //Propiedades
+        public string Nombre
+        {
+            get => this._nombre;
+            set
+            {
+                if (value.Trim().Length >= 5)
+                    this._nombre = value; //se acepta
+                else
+                    throw new ArgumentException("Nombre de ser de al menos 5 caracteres.");
+            }
+        }
+        public string Cuenta
+        {
+            get => this._cuenta;
+            set
+            {
+                if (value.Trim().Length == 11)
+                    this._cuenta = value;
+                else
+                    throw new ArgumentException("Cuenta debe ser de exactamente 11 caracteres");
+            }
+        }
+        public string DNI
+        {
+            get => this._dni;
+            set
+            {
+                if (value.Trim().Length == 13)
+                    this._dni = value;
+                else
+                    throw new ArgumentException("DNI debe ser de exactamente 13 caracteres");
+            }
+        }
+        public char Genero
+        {
+            get => this._genero;
+            set
+            {
+                if( value == 'M' || value == 'F' || value == 'X' )
+                    this._genero = value;
+                else
+                    throw new ArgumentException("Genero solo admite M, F ó X");
+            }    
+        }
+        public int Edad
+        {
+            get => this._edad;
+            set
+            {
+                if( value >= 12 )
+                    this._edad = value;
+                else
+                    throw new ArgumentException("Edad debe ser >= 12");
+            }
+        }
+        public float Promedio
+        {
+            get => this._promedio;
+            set
+            {
+                if( value >= 0 && value <= 100 )
+                    this._promedio = value;
+                else
+                    throw new ArgumentException("Promedio debe de estar entre 0 y 100");
+            }
+        }
+        public string Carrera
+        {
+            get => this._carrera;
+            set
+            {
+                if (value.Trim().Length >= 10)
+                    this._carrera = value; //se acepta
+                else
+                    throw new ArgumentException("Carrera de ser de al menos 10 caracteres.");
+            }
+        }
+        //Constructores
+        public Estudiante(string nombre, string cuenta, string dni, char genero, int edad, float promedio, string carrera)
+        {
+            this.Nombre = nombre;
+            this.Cuenta = cuenta;
+            this.DNI = dni;
+            this.Genero = genero;
+            this.Edad = edad;
+            this.Promedio = promedio;
+            this.Carrera = carrera;
+        }
+        //Metodos
+        public void Imprimir()
+        {
+            Console.WriteLine("****** Ficha de Estudiante ******");
+            Console.WriteLine($"Nombre: {this.Nombre}");
+            Console.WriteLine($"Cuenta: {this.Cuenta}");
+            Console.WriteLine($"DNI: {this.DNI}");
+            Console.WriteLine($"Genero: {this.Genero}");
+            Console.WriteLine($"Edad: {this.Edad}");
+            Console.WriteLine($"Promedio: {this.Promedio}");
+            Console.WriteLine($"Carrera: {this.Carrera}");
+        }
+    }
+}
